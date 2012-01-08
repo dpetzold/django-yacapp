610360f59b7d6bf5635d903059b3ef254291dd6a
Derrick Petzold <contact@derrickpetzold.com>
2012-01-07 19:35:11 -1000

    Comment rendering is working.

--- ajaxcomments/templatetags/ajaxcomments_tags.py
+++ ajaxcomments/templatetags/ajaxcomments_tags.py
@@ -27,19 +27,18 @@ class RenderComments(template.Node):
 #                object_pk_expr = parser.compile_filter(tokens[3])
 #            )
 
-    def get_target_ctype_pk(self, context):
-        if self.object_expr:
-            try:
-                obj = self.object_expr.resolve(context)
-            except template.VariableDoesNotExist:
-                return None, None
-            return ContentType.objects.get_for_model(obj), obj.pk
-        else:
-            return self.ctype, self.object_pk_expr.resolve(context, ignore_failures=True)
+
+    def render_comment(self, obj, comment, context):
+
+           text = render_to_string(
+                   'comment.html',
+                   {'comment': comment},
+                   context)
+           for child in obj.comments.filter(parent=comment):
+               text += self.render_comment(obj, child, context)
+           return text
 
     def render(self, context):
-
-        content_type, object_pk = self.get_target_ctype_pk(context)
 
         obj = self.object_expr.resolve(context)
 
@@ -47,9 +46,11 @@ class RenderComments(template.Node):
             parent__isnull=True,
             deleted__isnull=True)
 
-        form_str = render_to_string("comment.html", {'comment': comments[0]}, context)
-        context.pop()
-        return form_str
+        comments_text = ''
+        for comment in comments:
+            comments_text += self.render_comment(obj, comment, context)
+
+        return comments_text
 
 def render_comments(parser, token):
 
