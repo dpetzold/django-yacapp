c6139c6eef1b269fa603466d9a9129f166114075
Derrick Petzold <contact@derrickpetzold.com>
2012-01-08 16:13:13 -1000

    Reply recursion is working.

--- ajaxcomments/templatetags/ajaxcomments_tags.py
+++ ajaxcomments/templatetags/ajaxcomments_tags.py
@@ -1,8 +1,12 @@
 from django import template
 from django.template.loader import render_to_string
 from django.contrib.contenttypes.models import ContentType
+
+import logging
+
 register = template.Library()
 
+logger = logging.getLogger('django.ajaxcomments')
 
 class RenderComments(template.Node):
 
@@ -21,12 +25,18 @@ class RenderComments(template.Node):
             return cls(object_expr=parser.compile_filter(tokens[2]))
 
     def render_comment(self, obj, comment, context):
-       text = render_to_string(
-               'comment.html',
-               {'comment': comment},
-               context)
+
+       replies = ''
        for child in obj.comments.filter(parent=comment):
-           text += self.render_comment(obj, child, context)
+           replies += self.render_comment(obj, child, context)
+
+       text = render_to_string(
+               'comment.html', {
+                   'comment': comment,
+                   'replies': replies,
+                },
+               context)
+
        return text
 
     def render(self, context):
