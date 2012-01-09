a21bff9dc4541c0fb0e56ce634aa81ee7a80daab
Derrick Petzold <contact@derrickpetzold.com>
2012-01-08 20:30:53 -1000

    Reorged a bit.

--- ajaxcomments/media/comments.js
+++ ajaxcomments/media/comments.js
@@ -1,120 +1,114 @@
-function trim(s) {
-    s = s.replace(/(^\s*)|(\s*$)/gi,"");
-    s = s.replace(/[ ]{2,}/gi," ");
-    s = s.replace(/\n /,"\n");
-    return s;
-}
+(function() {
 
-function comment_edit(comment, event) {
+    window.comment_edit = function(comment, event) {
 
-  event.preventDefault();
+      event.preventDefault();
 
-  var comment_id = $(comment).attr('id').replace('edit', 'comment');
-  var comment = $('#' + comment_id + ' p');
-  $('#comment_id').attr('value', comment_id);
-  $('#id_edit').text(trim(comment.text()));
-  $('.comment-edit').dialog({width: 640, height: 700});
-}
-
-function comment_editsave(event) {
-
-  event.preventDefault();
-
-  var comment_id = $('#comment_id').val();
-  var p_id = comment_id.replace('comment', 'p');
-
-  $.post('/comment/edit/', {
-    'text': $('#id_edit').val(),
-    'comment_id': comment_id,
-    'object_pk': object_pk,
-    'content_type': content_type,
-    'csrfmiddlewaretoken': csrf_token,
-  }, function(response) {
-    if (response.success) {
-      $('#id_edit').val('');
-      $('.comment-edit').dialog('close');
-      $('#' + p_id).text(response.data.text);
+      var comment_id = $(comment).attr('id').replace('edit', 'comment');
+      var comment = $('#' + comment_id + ' p');
+      $('#comment_id').attr('value', comment_id);
+      $('#id_edit').text(comment.text());
+      $('.comment-edit').dialog({width: 640, height: 700});
     }
-    else {
-      $('.error').html('<h3>' + response.error + '</h3>');
+
+    window.comment_editsave = function(event) {
+
+      event.preventDefault();
+
+      var comment_id = $('#comment_id').val();
+      var p_id = comment_id.replace('comment', 'p');
+
+      $.post('/comment/edit/', {
+        'text': $('#id_edit').val(),
+        'comment_id': comment_id,
+        'object_pk': object_pk,
+        'content_type': content_type,
+        'csrfmiddlewaretoken': csrf_token,
+      }, function(response) {
+        if (response.success) {
+          $('#id_edit').val('');
+          $('.comment-edit').dialog('close');
+          $('#' + p_id).text(response.data.text);
+        }
+        else {
+          $('.error').html('<h3>' + response.error + '</h3>');
+        }
+      }, "json");
     }
-  }, "json");
-}
 
+    window.comment_delete = function(comment, event) {
 
-function comment_delete(comment, event) {
+      event.preventDefault();
 
-  event.preventDefault();
-
-  var comment_id = $(comment).attr('id').replace('delete', 'comment');
-  var comment = '#' + comment_id;
-  $.post('/comment/delete/', {
-      'comment_id': comment_id,
-      'csrfmiddlewaretoken': csrf_token,
-  }, function(response) {
-    $(comment).hide('fast');
-    container = $('.comments');
-    //$('html, body').animate({ scrollTop: container.offset().top }, 200);
-  });
-}
-
-function comment_reply(comment, event) {
-
-  event.preventDefault();
-
-  var comment_id = $(comment).attr('id').replace('reply', 'comment');
-  var comment = $('#' + comment_id + ' p');
-  $('#parent_id').attr('value', comment_id);
-  $('.comment-text').text(comment.html());
-  $("#dialog").dialog({width: 640, height: 800});
-}
-
-function comment_postreply(event) {
-
-  event.preventDefault();
-
-  var comment_id = $('#parent_id').attr('value');
-  $.post('/comment/post/', {
-    'text': $('#id_reply').val(),
-    'parent_id': $('#parent_id').val(),
-    'object_pk': object_pk,
-    'content_type': content_type,
-    'csrfmiddlewaretoken': csrf_token,
-  }, function(response) {
-    if (response.success) {
-      $('.comment-reply').dialog('close');
-      $('#id_reply').val('');
-      $('#' + comment_id).append(response.data.comment)
+      var comment_id = $(comment).attr('id').replace('delete', 'comment');
+      var comment = '#' + comment_id;
+      $.post('/comment/delete/', {
+          'comment_id': comment_id,
+          'csrfmiddlewaretoken': csrf_token,
+      }, function(response) {
+        $(comment).hide('fast');
+        container = $('.comments');
+        //$('html, body').animate({ scrollTop: container.offset().top }, 200);
+      });
     }
-    else {
-      $('.error').html('<h3>' + response.error + '</h3>');
+
+    window.comment_reply = function(comment, event) {
+
+      event.preventDefault();
+
+      var comment_id = $(comment).attr('id').replace('reply', 'comment');
+      var comment = $('#' + comment_id + ' p');
+      $('#parent_id').attr('value', comment_id);
+      $('.comment-text').text(comment.html());
+      $("#dialog").dialog({width: 640, height: 800});
     }
-  }, "json");
-}
 
-function comment_post(event) {
+    window.comment_postreply = function(event) {
 
-  event.preventDefault();
+      event.preventDefault();
 
-  $.post('/comment/post/', {
-    'text': $('#id_text').val(),
-    'object_pk': object_pk,
-    'content_type': content_type,
-    'csrfmiddlewaretoken': csrf_token,
-  }, function(response) {
-    if (response.success) {
-      $('.error').val('');
-      $('textarea').val('');
-      $('#id_title').val('');
-      $('.comment-form').hide();
-      $('.comments').append(response.data.comment)
+      var comment_id = $('#parent_id').attr('value');
+      $.post('/comment/post/', {
+        'text': $('#id_reply').val(),
+        'parent_id': $('#parent_id').val(),
+        'object_pk': object_pk,
+        'content_type': content_type,
+        'csrfmiddlewaretoken': csrf_token,
+      }, function(response) {
+        if (response.success) {
+          $('.comment-reply').dialog('close');
+          $('#id_reply').val('');
+          $('#' + comment_id).append(response.data.comment)
+        }
+        else {
+          $('.error').html('<h3>' + response.error + '</h3>');
+        }
+      }, "json");
     }
-    else {
-      $('.error').html('<h3>' + response.error + '</h3>');
-    }
-  }, "json");
 
-}
+    window.comment_post = function(event) {
+
+      event.preventDefault();
+
+      $.post('/comment/post/', {
+        'text': $('#id_text').val(),
+        'object_pk': object_pk,
+        'content_type': content_type,
+        'csrfmiddlewaretoken': csrf_token,
+      }, function(response) {
+        if (response.success) {
+          $('.error').val('');
+          $('textarea').val('');
+          $('#id_title').val('');
+          $('.comment-form').hide();
+          $('.comments').append(response.data.comment)
+        }
+        else {
+          $('.error').html('<h3>' + response.error + '</h3>');
+        }
+      }, "json");
+    }
+})();
 
 $(document).ready(function() {
 
